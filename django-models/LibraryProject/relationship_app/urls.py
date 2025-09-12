349ca10e307b788267fe8d5fb31deef433703c13
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-09-12 21:03:14 +0300

    Added the register view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import list_books,LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
+import views
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="registration_app/login.html"), name="login"),
     path('logout/',LogoutView.as_view(template_name="registration_app/logout.html"), name="logout"),
+    path('register/', views.register.as_view(), name="register"),
     path("books/", list_books, name="list_books"),  # function-based view
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,7 +8,7 @@ from .models import Book
 from .models import Library
 
 
-class SignUpView(CreateView):
+class register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration_app/register.html"
