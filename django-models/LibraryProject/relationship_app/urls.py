3dff02901e2edd608aa4e6d9945b58dbd4f562f4
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-09-12 21:00:32 +0300

    Added the template name in the logoutview

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="registration_app/login.html"), name="login"),
-    path('logout/',LogoutView.as_view(), name="logout"),
+    path('logout/',LogoutView.as_view(template_name="registration_app/logout.html"), name="logout"),
     path("books/", list_books, name="list_books"),  # function-based view
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
 ]
