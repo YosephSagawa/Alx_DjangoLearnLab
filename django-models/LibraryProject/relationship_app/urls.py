835b06f8b9cb2310833d30b84426be2c00bf5a59
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-09-12 22:02:19 +0300

    Added the role based access and the templates and views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('register/', views.register.as_view(), name="register"),
     path("books/", list_books, name="list_books"),  # function-based view
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
+    path("admin-role/", views.admin_view, name="admin_view"),
+    path("librarian-role/", views.librarian_view, name="librarian_view"),
+    path("member-role/", views.member_view, name="member_view"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .models import Book
 from .models import Library
 
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.userprofle.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
 
 class register(CreateView):
     form_class = UserCreationForm()
