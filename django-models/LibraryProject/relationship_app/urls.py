5516ee67ec46fd5a3616b54c53dcc5a90ee5781b
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-09-12 20:53:57 +0300

    Added the user authentication views and urls along with their templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import list_books,LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    path('login/', LoginView.as_view(template_name="registration_app/login.html"), name="login"),
+    path('logout/',LogoutView.as_view(), name="logout"),
     path("books/", list_books, name="list_books"),  # function-based view
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from .models import Book
 from .models import Library
 
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration_app/register.html"
 
 # Function-based view: List all books
 def list_books(request):
