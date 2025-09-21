c8e0ebb16d1ec5cb88ce0e91bf414f81530d03ee
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-09-21 21:30:35 +0300

    Added authentication and permission to access the APIs

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,8 +1,10 @@
-from rest_framework import serializers
+from rest_framework import serializers, viewsets
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = "__all__"
-        
+
+
+
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from .serializers import BookSerializer
 from .models import Book
-from rest_framework import generics, viewsets   
+from rest_framework import generics, viewsets  
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 
 # Create your views here.
 class BookList(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    authentication_classes = [IsAdminUser]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
