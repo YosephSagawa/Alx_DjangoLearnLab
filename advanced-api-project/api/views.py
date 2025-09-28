6c92a0215d49bdd9d3b10afc2ec8a132f3b3ea3b
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-09-28 21:28:14 +0300

    Added IsAuthenticated permission to views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,19 +3,19 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly, AllowAny, IsAuthenticated
 
 # Create your views here.
 class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = 'pk'
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class CreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
