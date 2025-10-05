e0dc2c97a0be44a8ad9ed3dd56a78d951065f157
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-10-05 20:44:28 +0300

    Added the comment model to each post

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('post/<int:post_pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
 
     # edit/delete specific comment by comment pk
-    path('comments/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
-    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
