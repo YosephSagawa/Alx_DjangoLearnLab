5625368f3a107f4d6812efb3bf52fbbedb7df58b
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-10-12 19:37:28 +0300

    Added feed and follower functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, FeedAPIView
+from django.urls import path
 
 
 router = DefaultRouter()
@@ -7,5 +8,7 @@ router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
 router.register(r'feed', FeedAPIView, basename='feed')
 
-
+urlpatterns = [
+    path('feed/', FeedAPIView.as_view(), name='feed'),
+]
 urlpatterns = router.urls
